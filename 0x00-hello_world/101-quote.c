fb60a23bfc8cba2df42f7d86e3b6d75c643557e2
ESSALLAMI <e.elmamia@gmail.com>
2023-03-16 14:52:33 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+ #include<stdio.h>
+       #include<unistd.h>
+    /**
+     * main-Entry point
+     *
+     * Return:Always1 (Success)
+     */
+     int main(void)
+     {
+          write(2,"with proper grammar, but the outcome is a piece of art,\n");
+          return(1);
+      }
+
