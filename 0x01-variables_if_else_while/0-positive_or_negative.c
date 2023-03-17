9efd72f26734d6632e39eef34bd5ae336c62aa5e
ESSALLAMI <e.elmamia@gmail.com>
2023-03-17 14:31:03 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,28 +2,27 @@
 #include<time.h>
 #include<stdio.h>
 
-/* if the number is greater than 0 is positive,if the number is 0 is rero */
-
-/* if the number is less than 0 is negative */
+/* if the number is greater than 0 is positive,if the number is 0 is rero
+ *
+ * if the number is less than 0 is negative */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        if (n>0)
-        {
+	n = rand()- RAND_MAX / 2;
+        if (n > 0)
+          {
             printf("%d is positive\n",n);
-         }
-         else if (n==0)
-         {
-            printf("%d is zero\n",n);
-         }
-          else
-         {
-             printf("%d is negative\n",n);
           }
+        else if (n == 0)
+          {
+            printf("%d is zero\n",n);
+          }
+         else
+          {
+             printf("%d is negative\n" ,n);
+           }
              
 	return (0);
 }
- 
