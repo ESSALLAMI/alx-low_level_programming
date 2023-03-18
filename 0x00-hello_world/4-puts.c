b98eaa2531d2d6a6ed054f4569a93c1ce51f8e3b
ESSALLAMI <e.elmamia@gmail.com>
2023-03-18 15:50:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
-* main-Entry point
-* Return:Always0(Success)
+* main- prints "Programming is like building a multilingual puzzle,
+*followed by a newline."
+* Return 0.
 */
 int main(void)
-{puts("Programming is like building a multilingual puzzle\n");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+} 
