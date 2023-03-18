7a07849e59f3ed6633c696c97e3e5e3d0a4609bf
ESSALLAMI <e.elmamia@gmail.com>
2023-03-18 08:55:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 * Return:Always0(Success)
 */
 int main(void)
-{puts("Programming is like building a multilingual puzzle");	
+{puts("\"Programming is like building a multilingual puzzle");	
 return (0);
 }
