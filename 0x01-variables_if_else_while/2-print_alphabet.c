0905faf7a8243158d29151b624715b8357dba56b
ESSALLAMI <e.elmamia@gmail.com>
2023-03-19 10:40:53 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - Prints the alphabetic
+*Return: Always (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
+
