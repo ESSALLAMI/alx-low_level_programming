7a7536127a72736eff6e6a9a29fca329acda704e
ESSALLAMI <e.elmamia@gmail.com>
2023-03-22 18:44:19 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  *startting with 0.
  *@n: The value of the times table to be printed.
  */
-void print_times_table(int n)
+void print_times_table(int n);
 {
 int num, mult, prod;
 if (n >= 0 && n <= 15)
