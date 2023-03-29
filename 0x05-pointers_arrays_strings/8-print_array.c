edaf0883e73c2f3909e93829be5f77cf123974e4
ESSALLAMI <e.elmamia@gmail.com>
2023-03-28 18:43:40 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,12 @@
 */
 void print_array(int *a, int n)
 {
-int i = 0;
+int i;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
-if (i < n - 1)
-printf(" , ");
+if (i != (n - 1))
+printf("%d", a[i]);
 }
 printf("\n");
 }
