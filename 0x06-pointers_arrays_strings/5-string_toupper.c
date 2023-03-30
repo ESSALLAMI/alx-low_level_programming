08609a9838881fa804841702f3eae0c5f5f925da
ESSALLAMI <e.elmamia@gmail.com>
2023-03-29 19:26:16 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*string_toupper - change all lowercase to yppercase
+*@str: the string to be the changed
+*Return:
+*/
+char *string_toupper(char *str)
+{
+int index = 0;
+while (str[index])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+index++;
+}
+return (str);
+}
