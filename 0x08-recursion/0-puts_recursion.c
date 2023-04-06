28d4704735eb977b0da5472e99199c2563882fde
ESSALLAMI <e.elmamia@gmail.com>
2023-04-06 00:14:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
 */
 void _puts_recursion(char *s)
 {
+s = "lpa\0";
 if (*s == '\0')
 {
 _putchar('\n');
+return;
 }
-else
-{
 _putchar(*s);
-_puts_recursion(s + 1);
-}
+s++;
+_puts_recursion(s);
 }
