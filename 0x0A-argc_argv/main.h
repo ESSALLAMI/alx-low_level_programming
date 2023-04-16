3a662d5115ed3903a10ba0a16357da8ca4e62e7b
ESSALLAMI <e.elmamia@gmail.com>
2023-04-15 17:21:55 -0700

    Updet

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-int main(int argc __attribute__((unused)), char *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
+int _putchar(char c);
 int _atoi(char *s);
 #endif
