afd6916c1bb06dffb0e197b44503ddd2773def51
ESSALLAMI <e.elmamia@gmail.com>
2023-05-11 23:49:38 -0700

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*create_buffer - Allocates 1024 bytes for a buffer.
+*@file: The name of the file buffer is storing chars
+*Return:A pointer to the newly-allocated buffer
+*/
+char *create_buffer(char *file)
+{
+char *buffer;
+buffer = malloc(sizeof(char) * 1024);
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO, "Error:Can't write to %s\n", file);
+exit(99);
+}
+return (buffer);
+}
+/**
+*close_file - Closes file descriptors
+*@fd: The file descriptor to be closed
+*/
+void close_file(int fd)
+{
+int c;
+c = close(fd);
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error:Can't write to %d\n", fd);
+exit(100);
+}
+}
+/**
+*main - copies the contentsof a file of anthor file
+*@argc: The num of arg
+*@argv: An array of pointers to the arg
+*Return:0 on success
+*Description: if the arg  count is incorrect - exit code 97
+*if file_from does not exist - exit code 98
+*if file_to cannot be created - exit code 99
+*if file_to or file_from cannot be closed - exit code 100
+*/
+int main(int argc, char *argv[])
+{
+int from, to, r, w;
+char *buffer;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage:cp file_from file_to\n");
+exit(97);
+}
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+if (from == -1 || r == -1)
+{
+dprintf(STDERR_FILENO, "Error:Can't read file %s\n", argv[1]);
+free(buffer);
+exit(98);
+}
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
+{
+dprintf(STDERR_FILENO, "Error:Can't write %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
+}
+while (r > 0);
+free(buffer);
+close_file(from);
+close_file(to);
+return (0);
+}
