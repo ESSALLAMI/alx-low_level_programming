39a848dce01b02df813e150916cf4949419e0d4d
ESSALLAMI <e.elmamia@gmail.com>
2023-05-11 18:02:15 -0700

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len]; len++)
+for (len = 0; text_content[len];)
 len++;
 }
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
